dd92911dd66e38dde71dd6a795258f27f40b863a
Carolyne munyiva <munyivacarolyne571@gmail.com>
2024-04-25 08:33:13 +0300

    complete destructure

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,12 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>
+           {/* Map over the array of links and create an <a> tag for each */}
+           {links.map((link, index) => (
+           <a key={index} href={`#${link}`}>{link}</a>
+      ))}
+         </nav>;
 }
 
 export default NavBar;
